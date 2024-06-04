3224cebf9ad5e1968fbebda2536ef03ee97daa6e
ceron-eng <ceronventura.alexis45@gmail.com>
2024-06-03 19:39:17 -0600

    conexión a mysql

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using ApiMicroservice.Autor.Aplication;
 using ApiMicroservice.Autor.Persistencia;
+using ApiMicroservice.Autor.Aplication;
+using ApiMicroservice.Autor.Persistencia;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
@@ -13,19 +19,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DbContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 27)) // Ajusta la versión del servidor MySQL según corresponda
-    ));
+
+builder.Services.AddControllers()
+    .AddFluentValidation
+    (cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
+
+builder.Services.AddDbContext<ContextoAutor>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 builder.Services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
 builder.Services.AddAutoMapper(typeof(Consulta.Manejador));
-builder.Services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
